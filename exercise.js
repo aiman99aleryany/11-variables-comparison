f00177668670ab9630a7cc90bd4a6245e26bcc15
aiman99aleryany <102668388+aiman99aleryany@users.noreply.github.com>
2022-09-16 17:33:30 +0300

    init commit

---
+++ exercise.js
@@ -0,0 +1,19 @@
+// # Exercise 11
+
+// Modify the second term of the expression in order to get true as result.
+
+// 10 < 9
+// 45 == 34
+// true === 1
+// false === 0
+// 1 == 0
+// 4 <= 2
+// 4 >= 8
+
+10 < 11; // 10 < 10;
+45 == 45; // 45 == 45;
+true === true; // true === true;
+false === false; // false === false;
+1 == 1; // 1 == 1;
+4 <= 4; // 4 <= 4;
+4 >= 4; // 4 >= 4;
